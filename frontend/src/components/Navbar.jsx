640928cea3d6a95d73339cfb1a230ceaed65ccec
unknown <mustafausakli_@hotmail.com>
2022-09-17 06:16:50 +0300

    navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "../styles/navbar.scss"
+import { AiOutlineSearch, AiOutlineShoppingCart } from "react-icons/ai"
+import { MdOutlineLanguage } from "react-icons/md"
+
+const Navbar = () => {
+    return (
+        <header className='header-container'>
+            <div className="header-wrapper">
+                <div className='left'>
+                    <div className='language'> <MdOutlineLanguage />TR</div>
+                    <div className='search'>
+                        <input placeholder='sdas' type="text" />
+                        <AiOutlineSearch className='search-icon' />
+                    </div>
+                </div>
+                <div className='center'>
+                    <h1 className='logo'>aelita.</h1>
+                </div>
+                <div className='right'>
+                    <div className="menu-item">REGISTER</div>
+                    <div className="menu-item">SIGN IN</div>
+                    <div className="menu-item notification"><AiOutlineShoppingCart />
+                        <span className='badge'>3</span>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Navbar
---
+++ frontend/src/components/Slider.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/slider.scss"
+
+const Slider = () => {
+    return (
+        <div className='slider-container'>
+
+        </div>
+    )
+}
+
+export default Slider
